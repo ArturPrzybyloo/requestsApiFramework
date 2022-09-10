36a6d3e8f5c8323683f851a5d8d7f85cea3a9d37
Artur Przybyło <a.przybyloo@gmail.com>
2022-09-10 15:39:05 +0200

    Added book tests

--- tests/base_test.py
+++ tests/base_test.py
@@ -4,6 +4,7 @@ from session.session_provider import SessionProvider
 
 
 class TestBase:
-        fake = Faker('en_US')
-        session, user_id = SessionProvider().new_user_session(fake.first_name() + fake.last_name(),
-                                                              "aaaAAA123@")
+    fake = Faker('en_US')
+    new_user_session, user_id = SessionProvider().new_user_session(fake.first_name() + fake.last_name(), "aaaAAA123@")
+    session = SessionProvider().default_session()
+
