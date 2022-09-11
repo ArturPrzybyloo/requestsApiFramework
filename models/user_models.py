0ed3e33ff70a90320fd0d8c929abcf9fba1ef51e
Artur Przybyło <a.przybyloo@gmail.com>
2022-09-11 11:35:34 +0200

    Added books tests

--- models/user_models.py
+++ models/user_models.py
@@ -4,7 +4,7 @@ import requests
 
 from helpers.assertion_utils import verify_response_status_code
 from helpers.status_codes import StatusCodes
-from models.book_models import Book
+from models import book_models
 from models.utils_models import MessageModal, TokenViewModel
 from requestsUtils.endpoint_builder import EndpointBuilder
 
@@ -78,14 +78,14 @@ class GetUserResult:
         self.username = username
         self.books = books
 
-
     @classmethod
     def from_json(cls, json_dict):
         json_string = json.dumps(json_dict)
         json_dict = json.loads(json_string)
         books_objects = []
-        for book in json_dict['books']:
-            books_objects.append(Book.from_json(book))
+        if json_dict['books'] is not None:
+            for book in json_dict['books']:
+                books_objects.append(book_models.Book.from_json(book))
         return cls(userId=json_dict['userId'],
                    username=json_dict['username'],
                    books=books_objects)
